867835f5d74928717b16094e29455829e0500423
metehankarabag <metehankarabag1992@gmail.com>
2016-04-07 14:54:23 +0300

    uygulama dosyaları

---
+++ BosOtopark/ParkingLot.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BosOtopark
+{
+    public class ParkingLot
+    {
+        private List<Araba> car = new List<Araba>();
+        private int parkingLotSize;
+        private string parkingLotName;
+        
+        public int ParkingLotSize
+        {
+            get { return parkingLotSize; }
+            set { parkingLotSize = value; }
+        }
+        public string ParkingLotName
+        {
+            get { return parkingLotName; }
+            set { parkingLotName = value; }
+        }
+        
+        public ParkingLot()
+        {
+            car.Capacity = parkingLotSize;
+        }
+        public ParkingLot(string PlName, int PlSize)
+        {
+            ParkingLotName = PlName;
+            ParkingLotSize = PlSize;
+            car.Capacity = parkingLotSize;
+        }
+        public void InsertCarToTheParkingLot(ref Araba a)
+        {
+            if (car.Capacity > car.Count && a != null)
+            {
+                car.Add(a);
+                Console.WriteLine("there is/are {0} empty place(s)", car.Capacity - car.Count);
+                a = null;
+            }
+            else
+            {
+                Console.WriteLine("The parking lot is full");
+            }
+        }
+    }
+    public class ParkingLotCollection
+    {
+        List<ParkingLot> parkingLots = new List<ParkingLot>();
+
+        public List<ParkingLot> ParkingLots
+        {
+            get { return parkingLots; }
+            set { parkingLots = value; }
+        }
+
+        public ParkingLot this[string _parkingLotName]
+        {
+            get
+            {
+                if (parkingLots.Count != 0)
+                {
+                    return parkingLots.Find(x => x.ParkingLotName == _parkingLotName);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ BosOtopark/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BosOtopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingLotCollection pCollection = new ParkingLotCollection();
+            while (true)
+            {
+                Console.WriteLine("Do you want to add a parking lot? -y or -n");
+                string yesOrNo = Console.ReadLine();
+                if (yesOrNo == "y")
+                {
+                    Console.WriteLine("Enter Parking lot name:");
+                    string parkingLotName = Console.ReadLine();
+                    Console.WriteLine("Ender Parking lot capacity:");
+                    int parkingLotCapacity = Convert.ToInt32(Console.ReadLine());
+                    pCollection.ParkingLots.Add(new ParkingLot(parkingLotName, parkingLotCapacity));
+                }
+                else if (yesOrNo == "n" && pCollection.ParkingLots.Count == 0)
+                {
+                    Console.WriteLine("Ther is no parking lots");
+                    continue;
+                }
+
+                Console.WriteLine("Do you want to park? -y or -n");
+                yesOrNo = Console.ReadLine();
+                if (yesOrNo == "y")
+                {
+                    string question = "Which parking lot do you want to park? - ";
+                    foreach (ParkingLot item in pCollection.ParkingLots)
+                    {
+                        question += item.ParkingLotName + " - ";
+                    }
+                    Console.WriteLine(question);
+                    string parkingLot = Console.ReadLine();
+                    ParkingLot p = pCollection[parkingLot];
+                    if (p != null)
+                    {
+                        Araba a = new Araba();
+                        p.InsertCarToTheParkingLot(ref a);
+                    }
+                    else
+                        Console.WriteLine("Ther is no parking lot with name = {0} ", parkingLot);
+
+                }
+            }
+        }
+    }
+}
